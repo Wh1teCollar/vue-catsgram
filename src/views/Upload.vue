66d1464224ef3664500925dbd3c7c78c14ae6dd1
Mikhail Glushchenko <mike.glushchenko@gmail.com>
2021-12-25 21:38:24 +0200

    the page is under construction

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
     <h1 class="title">Upload</h1>
+    <p>The page is under construction</p>
+    <Loader v-if="isLoading" />
     <Body />
   </div>
 </template>
 
 <script>
 import Body from "@/components/Body";
+import Loader from "@/components/Loader";
 export default {
+  data() {
+    return {
+      isLoading: true,
+    };
+  },
   components: {
     Body,
+    Loader,
   },
 };
 </script>
