73f6b7366ef16bccd8713ebcbb95e0eced29c94c
Mikhail Glushchenko <mike.glushchenko@gmail.com>
2021-12-25 17:29:27 +0200

    Edit color

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@ export default {
 
 <style scope>
 html {
-  background-image: linear-gradient(to top, #f43b47 0%, #453a94 100%);
+  background-image: linear-gradient(to top, #9795f0 0%, #fbc8d4 100%);
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
