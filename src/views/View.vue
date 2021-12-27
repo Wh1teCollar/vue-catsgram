23b1bd1e2d39d5915527ed538f74b2297e55d0b4
Mikhail Glushchenko <mike.glushchenko@gmail.com>
2021-12-27 13:49:48 +0200

    View page added

--- src/views/View.vue
+++ src/views/View.vue
@@ -1,24 +1,38 @@
 <template>
-  <div>
-    <h1 class="title">View</h1>
-    <p>The page is under construction</p>
-    <Loader v-if="isLoading" />
-  </div>
+    <div>
+        <h1 class="title">View</h1>
+        <Loader v-if="isLoading" />
+        <Body v-bind:catsList="catsList" />
+    </div>
 </template>
 
 <script>
-import Loader from "@/components/Loader";
+import axios from 'axios';
+import Body from '@/components/Body';
+import Loader from '@/components/Loader';
 export default {
-  data() {
-    return {
-      isLoading: true,
-    };
-  },
-  components: {
-    Loader,
-  },
+    mounted() {
+        const headers = { 'x-api-key': 'db2157d0-39de-439e-b8fe-7e033060a0f0' };
+        axios
+            .get('https://api.thecatapi.com/v1/images/', {
+                headers,
+            })
+            .then((response) => {
+                this.catsList = response.data;
+                this.isLoading = false;
+            });
+    },
+    data() {
+        return {
+            catsList: [],
+            isLoading: true,
+        };
+    },
+    components: {
+        Body,
+        Loader,
+    },
 };
 </script>
 
-<style scope>
-</style>
+<style scope></style>
